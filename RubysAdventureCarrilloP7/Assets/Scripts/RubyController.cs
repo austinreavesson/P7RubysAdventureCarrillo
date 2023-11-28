b1e1063248f4152e5ce675db40c233631649356d
Alejandro Carrillo <718269@student.dusd.net>
2023-11-28 14:02:06 -0800

    idk
    came back and was doing my normal thing and saw this

--- RubysAdventureCarrilloP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureCarrilloP7/Assets/Scripts/RubyController.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
         
 
         Vector2 position = transform.position;
